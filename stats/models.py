1ccc9f0c5593ac330852c3f571e66471d7fbcd1c
nour1029 <nour.m7amad.ezeem@gmail.com>
2023-02-09 06:39:14 +0200

    create models

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.utils.translation import gettext as _
+from django.utils.text import slugify
 # Create your models here.
+
+
+class Statistic(models.Model):
+    name = models.CharField(_("Name"), max_length=50)
+    slug = models.SlugField(_("Slug"), blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    @property
+    def data(self):
+        return self.dataitem_set.all()
+
+    def __str__(self):
+        return self.name
+
+
+class DataItem(models.Model):
+    statstic = models.ForeignKey(Statistic, related_name='statistic_dataitems', verbose_name=_("Statistic"), on_delete=models.CASCADE)
+    value = models.PositiveSmallIntegerField(_("Value"))
+    user = models.CharField(_("User"), max_length=50)
+
+    def __str__(self):
+        return f"{self.user} - {self.value}"
